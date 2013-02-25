cc355e8439571571acdb96441a1c14943fa8cfbc
haul <h4ul@me.com>
2013-02-25 22:41:05 +0400

    Update app/controllers/spree/gateway/robokassa_controller.rb

--- app/controllers/spree/gateway/robokassa_controller.rb
+++ app/controllers/spree/gateway/robokassa_controller.rb
@@ -2,6 +2,7 @@ class Spree::Gateway::RobokassaController < Spree::CheckoutController
   skip_before_filter :verify_authenticity_token, :only => [:result, :success, :fail]
   skip_before_filter :ensure_valid_state, :only => [:result, :success, :fail]
   skip_before_filter :load_order, :only => [:result, :success, :fail]
+  ssl_required
 
   def show
     @order =  Spree::Order.find(params[:order_id])
